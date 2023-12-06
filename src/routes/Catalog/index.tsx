650d300ffa70af2aedafaa77c9a3bce6bb931459
newrodrigo <rodrigocerqueira@outlook.com>
2023-12-05 22:01:14 -0300

    Props catalog card

--- src/routes/Catalog/index.tsx
+++ src/routes/Catalog/index.tsx
@@ -3,6 +3,26 @@ import HeaderClient from "../../components/HeaderClient";
 import SearchBar from "../../components/SearchBar";
 import CatalogCard from "../../components/CatalogCard";
 import ButtonNextPage from "../../components/ButtonNextPage";
+import { ProductDTO } from "../../models/product";
+
+const product: ProductDTO = {
+  id: 2,
+  name: "Smart TV",
+  description: "TV TOP",
+  price: 2500.99,
+  imgUrl:
+    "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg",
+  categories: [
+    {
+      id: 1,
+      name: "Eletrônicos",
+    },
+    {
+      id: 1,
+      name: "Importados",
+    },
+  ],
+};
 
 export default function Catalog() {
   return (
@@ -13,17 +33,16 @@ export default function Catalog() {
           <SearchBar />
 
           <div className="dsc-catalog-cards dsc-mt20 dsc-mb20">
-            <CatalogCard />
-            <CatalogCard />
-            <CatalogCard />
-            <CatalogCard />
-            <CatalogCard />
-            <CatalogCard />
-            <CatalogCard />
-            <CatalogCard />
-            <CatalogCard />
-            <CatalogCard />
-            <CatalogCard />
+            <CatalogCard product={product} />
+            <CatalogCard product={product} />
+            <CatalogCard product={product} />
+            <CatalogCard product={product} />
+            <CatalogCard product={product} />
+            <CatalogCard product={product} />
+            <CatalogCard product={product} />
+            <CatalogCard product={product} />
+            <CatalogCard product={product} />
+            <CatalogCard product={product} />
           </div>
 
           <ButtonNextPage />
