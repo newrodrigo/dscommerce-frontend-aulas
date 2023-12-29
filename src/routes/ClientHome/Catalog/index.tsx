b48f5494b4316600cd9caa44a2cf1eb3a7ea54d3
newrodrigo <rodrigocerqueira@outlook.com>
2023-12-28 23:22:25 -0300

    First back end requests

--- src/routes/ClientHome/Catalog/index.tsx
+++ src/routes/ClientHome/Catalog/index.tsx
@@ -3,15 +3,24 @@ import ButtonNextPage from "../../../components/ButtonNextPage";
 import CatalogCard from "../../../components/CatalogCard";
 import SearchBar from "../../../components/SearchBar";
 import * as productService from "../../../services/product-service";
+import { useEffect, useState } from "react";
 
 export default function Catalog() {
+  const [products, setProducts] = useState<ProductDTO[]>([]);
+
+  useEffect(() => {
+    productService.findAll().then((response) => {
+      setProducts(response.data.content);
+    });
+  }, []);
+
   return (
     <main>
       <section id="catalog-section" className="dsc-container">
         <SearchBar />
 
         <div className="dsc-catalog-cards dsc-mt20 dsc-mb20">
-          {productService.findAll().map((product) => (
+          {products.map((product) => (
             <CatalogCard key={product.id} product={product} />
           ))}
         </div>
--- src/routes/ClientHome/ProductDetails/index.tsx
+++ src/routes/ClientHome/ProductDetails/index.tsx
@@ -5,11 +5,19 @@ import ProductDetailsCard from "../../../components/ProductDetailsCard";
 import * as productService from "../../../services/product-service";
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { ProductDTO } from "../../../models/product";
 
 export default function ProductDetails() {
   const params = useParams();
 
-  const product = productService.findById(Number(params.productId));
+  const [product, setProduct] = useState<ProductDTO>();
+
+  useEffect(() => {
+    productService.findById(Number(params.productId)).then((response) => {
+      setProduct(response.data);
+    });
+  }, []);
 
   return (
     <main>
