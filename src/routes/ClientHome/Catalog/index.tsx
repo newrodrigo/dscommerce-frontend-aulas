436a42bf6ffadc5c24fc7600a963110527d28e64
newrodrigo <rodrigocerqueira@outlook.com>
2024-01-11 21:11:04 -0300

    Cart mock data

--- src/routes/ClientHome/Catalog/index.tsx
+++ src/routes/ClientHome/Catalog/index.tsx
@@ -4,6 +4,7 @@ import CatalogCard from "../../../components/CatalogCard";
 import SearchBar from "../../../components/SearchBar";
 import * as productService from "../../../services/product-service";
 import { useEffect, useState } from "react";
+import { ProductDTO } from "../../../models/product";
 
 export default function Catalog() {
   const [products, setProducts] = useState<ProductDTO[]>([]);
