cc3b255bda1c35f060682d2d8751c4ff0688d187
newrodrigo <rodrigocerqueira@outlook.com>
2024-01-21 17:48:57 -0300

    Context cart count

--- src/components/CartIcon/index.tsx
+++ src/components/CartIcon/index.tsx
@@ -1,11 +1,17 @@
 import "./styles.css";
 import cartIcon from "../../assets/cart.svg";
+import { useContext } from "react";
+import { ContextCartCount } from "../../utils/context-cart";
 
 export default function CartIcon() {
+  const { contextCartCount } = useContext(ContextCartCount);
+
   return (
     <>
       <img src={cartIcon} alt="Carrinho de compras" />
-      <div className="dsc-cart-count">2</div>
+      {contextCartCount > 0 && (
+        <div className="dsc-cart-count">{contextCartCount}</div>
+      )}
     </>
   );
 }
