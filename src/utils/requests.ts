3f3af0da88e27274a58c88d22cb1e9d3fdc4603e
newrodrigo <rodrigocerqueira@outlook.com>
2024-02-15 11:23:34 -0300

    Login form, login request

---
+++ src/utils/requests.ts
@@ -0,0 +1,8 @@
+import axios, { AxiosRequestConfig } from "axios";
+import { BASE_URL } from "./system";
+
+export function requestBackend(config: AxiosRequestConfig) {
+
+    return axios({...config, baseURL: BASE_URL});
+
+}
