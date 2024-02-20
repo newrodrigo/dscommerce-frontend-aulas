cbbee4e903137eb67ed0165bf3d0fbc8c0e449ec
newrodrigo <rodrigocerqueira@outlook.com>
2024-02-20 18:36:43 -0300

    Admin area routes

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,8 @@ import { ContextToken } from "./utils/context-token";
 import * as authService from "./services/auth-service";
 import * as cartService from "./services/cart-service";
 import Confirmation from "./routes/ClientHome/Confirmation";
+import ProductListing from "./routes/Admin/ProductListing";
+import ProductForm from "./routes/Admin/ProductForm";
 
 export default function App() {
   const [contextCartCount, setContextCartCount] = useState<number>(0);
@@ -67,7 +69,10 @@ export default function App() {
                 </PrivateRoute>
               }
             >
-              <Route index element={<AdminHome />} />
+              <Route index element={<Navigate to="/admin/home" />} />
+              <Route path="home" element={<AdminHome />} />
+              <Route path="products" element={<ProductListing />} />
+              <Route path="products/:productId" element={<ProductForm />} />
             </Route>
             <Route path="*" element={<Navigate to="/" />} />
           </Routes>
---
+++ src/routes/Admin/ProductListing/index.tsx
@@ -0,0 +1,3 @@
+export default function ProductListing() {
+  return <h1>Listagem de produtos</h1>;
+}
