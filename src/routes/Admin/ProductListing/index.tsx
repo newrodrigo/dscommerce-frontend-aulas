4214b73d4a5562bb515641240d1164c20aea6935
newrodrigo <rodrigocerqueira@outlook.com>
2024-02-20 19:04:24 -0300

    Product CRUD static layout

--- src/routes/Admin/ProductListing/index.tsx
+++ src/routes/Admin/ProductListing/index.tsx
@@ -1,3 +1,119 @@
+import "./styles.css";
+import editIcon from "../../../assets/edit.svg";
+import deleteIcon from "../../../assets/edit.svg";
+import product from "../../../assets/computer.png";
+
 export default function ProductListing() {
-  return <h1>Listagem de produtos</h1>;
+  return (
+    <main>
+      <section id="product-listing-section" className="dsc-container">
+        <h2 className="dsc-section-title dsc-mb20">Cadastro de produtos</h2>
+
+        <div className="dsc-btn-page-container">
+          <div className="dsc-btn dsc-btn-white dsc-mb20">Novo</div>
+        </div>
+
+        <form className="dsc-search-bar">
+          <button type="submit">🔎︎</button>
+          <input type="text" placeholder="Nome do produto" />
+          <button type="reset">🗙</button>
+        </form>
+
+        <table className="dsc-table dsc-mt20 dsc-mb20">
+          <thead>
+            <tr>
+              <th className="dsc-tb576">ID</th>
+              <th></th>
+              <th className="dsc-tb768">Preço</th>
+              <th className="dsc-txt-left">Nome</th>
+              <th></th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className="dsc-tb576">341</td>
+              <td>
+                <img
+                  className="dsc-product-listing-image"
+                  src={product}
+                  alt="Computer"
+                />
+              </td>
+              <td className="dsc-tb768">R$ 5000,00</td>
+              <td className="dsc-txt-left">Computador Gamer XT Plus Ultra</td>
+              <td>
+                <img
+                  className="dsc-product-listing-btn"
+                  src={editIcon}
+                  alt="Editar"
+                />
+              </td>
+              <td>
+                <img
+                  className="dsc-product-listing-btn"
+                  src={deleteIcon}
+                  alt="Deletar"
+                />
+              </td>
+            </tr>
+            <tr>
+              <td className="dsc-tb576">341</td>
+              <td>
+                <img
+                  className="dsc-product-listing-image"
+                  src={product}
+                  alt="Computer"
+                />
+              </td>
+              <td className="dsc-tb768">R$ 5000,00</td>
+              <td className="dsc-txt-left">Computador Gamer XT Plus Ultra</td>
+              <td>
+                <img
+                  className="dsc-product-listing-btn"
+                  src={editIcon}
+                  alt="Editar"
+                />
+              </td>
+              <td>
+                <img
+                  className="dsc-product-listing-btn"
+                  src={deleteIcon}
+                  alt="Deletar"
+                />
+              </td>
+            </tr>
+            <tr>
+              <td className="dsc-tb576">341</td>
+              <td>
+                <img
+                  className="dsc-product-listing-image"
+                  src={product}
+                  alt="Computer"
+                />
+              </td>
+              <td className="dsc-tb768">R$ 5000,00</td>
+              <td className="dsc-txt-left">Computador Gamer XT Plus Ultra</td>
+              <td>
+                <img
+                  className="dsc-product-listing-btn"
+                  src={editIcon}
+                  alt="Editar"
+                />
+              </td>
+              <td>
+                <img
+                  className="dsc-product-listing-btn"
+                  src={deleteIcon}
+                  alt="Deletar"
+                />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+
+        <div className="dsc-btn-next-page">Carregar mais</div>
+      </section>
+    </main>
+  );
 }
