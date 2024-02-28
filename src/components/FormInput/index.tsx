f97feea5a6ee0131b5e5171f4544c46e116b2229
newrodrigo <rodrigocerqueira@outlook.com>
2024-02-27 22:12:11 -0300

    Error CSS

--- src/components/FormInput/index.tsx
+++ src/components/FormInput/index.tsx
@@ -1,5 +1,5 @@
 export default function FormInput(props: any) {
-  const { validation, ...inputProps } = props;
+  const { validation, invalid, ...inputProps } = props;
 
-  return <input {...inputProps} />;
+  return <input {...inputProps}  data-invalid={invalid} />;
 }
--- src/routes/Admin/ProductForm/index.tsx
+++ src/routes/Admin/ProductForm/index.tsx
@@ -19,7 +19,7 @@ export default function ProductForm() {
       placeholder: "Nome",
     },
     price: {
-      value: "200",
+      value: "",
       id: "price",
       name: "price",
       type: "number",
@@ -39,9 +39,6 @@ export default function ProductForm() {
   });
 
   useEffect(() => {
-    const obj = forms.validate(formData, "price");
-    console.log(obj);
-
     if (isEditing)
       productService.findById(Number(params.productId)).then((response) => {
         setFormData(forms.updateAll(formData, response.data));
@@ -49,7 +46,13 @@ export default function ProductForm() {
   }, []);
 
   function handleInputChange(event: any) {
-    setFormData(forms.update(formData, event.target.name, event.target.value));
+    const dataUpdated = forms.update(
+      formData,
+      event.target.name,
+      event.target.value
+    );
+    const dataValidated = forms.validate(dataUpdated, event.target.name);
+    setFormData(dataValidated);
   }
 
   return (
@@ -65,6 +68,7 @@ export default function ProductForm() {
                   className="dsc-form-control"
                   onChange={handleInputChange}
                 />
+                <div className="dsc-form-error">{formData.name.message}</div>
               </div>
               <div>
                 <FormInput
@@ -72,6 +76,7 @@ export default function ProductForm() {
                   className="dsc-form-control"
                   onChange={handleInputChange}
                 />
+                <div className="dsc-form-error">{formData.price.message}</div>
               </div>
               <div>
                 <FormInput
