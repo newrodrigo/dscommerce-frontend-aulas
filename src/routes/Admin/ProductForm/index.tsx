08100f98a0f25a2095c24998b2193bee9738c2de
newrodrigo <rodrigocerqueira@outlook.com>
2024-02-27 21:48:41 -0300

    forms validate

--- src/routes/Admin/ProductForm/index.tsx
+++ src/routes/Admin/ProductForm/index.tsx
@@ -19,11 +19,15 @@ export default function ProductForm() {
       placeholder: "Nome",
     },
     price: {
-      value: "",
+      value: "200",
       id: "price",
       name: "price",
       type: "number",
       placeholder: "Preço",
+      validation: function (value: any) {
+        return Number(value) > 0;
+      },
+      message: "Favor informar um valor positivo",
     },
     imgUrl: {
       value: "",
@@ -35,6 +39,9 @@ export default function ProductForm() {
   });
 
   useEffect(() => {
+    const obj = forms.validate(formData, "price");
+    console.log(obj);
+
     if (isEditing)
       productService.findById(Number(params.productId)).then((response) => {
         setFormData(forms.updateAll(formData, response.data));
